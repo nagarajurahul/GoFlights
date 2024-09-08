91a3e619bacad1eb0b50cc28d0f77f3909ab9cc6
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-09-08 16:54:11 -0500

    Flight Controller and other services added

--- src/main/java/com/rnagaraju/goflights/controller/FlightController.java
+++ src/main/java/com/rnagaraju/goflights/controller/FlightController.java
@@ -1,16 +1,30 @@
 package com.rnagaraju.goflights.controller;
 
 
+import com.rnagaraju.goflights.model.Flight;
+import com.rnagaraju.goflights.service.FlightService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class FlightController {
+
+    @Autowired
+    private FlightService flightService;
 
     @GetMapping("/goflights")
     @ResponseBody
     public String goflights() {
         return "Hello, Welcome to our GoFlights application!";
+    }
+
+    @GetMapping("/flights")
+    @ResponseBody
+    public List<Flight> getFlights(){
+        return flightService.getAllFlights();
     }
 }
--- src/main/java/com/rnagaraju/goflights/service/FlightService.java
+++ src/main/java/com/rnagaraju/goflights/service/FlightService.java
@@ -5,14 +5,22 @@ import com.rnagaraju.goflights.repository.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class FlightService extends AbstractService<Flight> {
 
     @Autowired
     FlightRepository flightRepository;
 
+    public List<Flight> getAllFlights() {
+        List<Flight> flights = flightRepository.findAll();
+        return flights;
+    }
+
     public Flight createFlight(Long id) {
         Flight flight = flightRepository.findById(id).orElse(null);
         return flight;
     }
+
 }
