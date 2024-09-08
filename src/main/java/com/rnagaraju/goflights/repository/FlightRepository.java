d9a18948e83aa96c661503bfbaf0251b37cd9e24
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-09-08 16:38:09 -0500

    Spring Data JPA added to pom.xml dependency and FlightRepository updated

--- src/main/java/com/rnagaraju/goflights/repository/FlightRepository.java
+++ src/main/java/com/rnagaraju/goflights/repository/FlightRepository.java
@@ -1,4 +1,8 @@
 package com.rnagaraju.goflights.repository;
 
-public class FlightRepository {
+import com.rnagaraju.goflights.model.Flight;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface FlightRepository extends JpaRepository<Flight, Long> {
 }
