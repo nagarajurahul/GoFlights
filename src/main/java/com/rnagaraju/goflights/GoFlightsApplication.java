4b7873d6dc6a5943d10178c322027607189d0c6f
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-09-08 15:54:15 -0500

    Main program for SpringBootApplication

---
+++ src/main/java/com/rnagaraju/goflights/GoFlightsApplication.java
@@ -0,0 +1,11 @@
+package com.rnagaraju.goflights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoFlightsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GoFlightsApplication.class, args);
+    }
+}
