59d24ed8e8479e2ab6a7a1c792053c02ee30e5c0
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-09-10 13:23:00 -0500

    Code Resolved to use Spring's DataIntegrityViolationException instead of custom

--- src/main/java/com/rnagaraju/goflights/exception/DataIntegrityViolationException.java
+++
@@ -1,7 +0,0 @@
-package com.rnagaraju.goflights.exception;
-
-public class DataIntegrityViolationException extends RuntimeException {
-    public DataIntegrityViolationException(String message) {
-        super(message);
-    }
-}
