69622ec641f09d9d9026a36ce8a3719fee672f2e
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-09-17 16:20:03 -0500

    Added RestTemplate

--- src/main/java/com/rnagaraju/goflights/service/user/UserBookingService.java
+++ src/main/java/com/rnagaraju/goflights/service/user/UserBookingService.java
@@ -1,17 +1,23 @@
 package com.rnagaraju.goflights.service.user;
 
+import com.rnagaraju.goflights.dto.PassengerDTO;
 import com.rnagaraju.goflights.dto.user.CreateUserBookingDTO;
 import com.rnagaraju.goflights.dto.user.UserBookingDTO;
+import com.rnagaraju.goflights.dto.user.UserFlightDTO;
 import com.rnagaraju.goflights.mapper.user.UserBookingMapper;
 import com.rnagaraju.goflights.model.Booking;
 import com.rnagaraju.goflights.repository.user.UserBookingRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @Service
 public class UserBookingService {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Autowired
     private UserBookingRepository bookingRepository;
@@ -33,7 +39,19 @@ public class UserBookingService {
     }
 
     public UserBookingDTO createBooking(CreateUserBookingDTO bookingDTO) {
-        
+
+        Booking booking=new Booking();
+
+        String flightUrl="http://user/flights/";
+        flightUrl+=bookingDTO.getFlightId();
+        UserFlightDTO flight=restTemplate.getForObject(flightUrl, UserFlightDTO.class);
+
+        String passengerUrl="http://user/passengers/";
+        passengerUrl+=bookingDTO.getPassengerId();
+        PassengerDTO passenger=restTemplate.getForObject(passengerUrl, PassengerDTO.class);
+
+        bookingRepository.save(booking);
+
         return null;
     }
 }
