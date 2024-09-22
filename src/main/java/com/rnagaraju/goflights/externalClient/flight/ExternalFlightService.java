2747444bdefee5af3bb78aadf44098ee51baac34
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-09-22 13:51:29 -0500

    Slight modifications 3 - external client

--- src/main/java/com/rnagaraju/goflights/externalClient/flight/ExternalFlightService.java
+++ src/main/java/com/rnagaraju/goflights/externalClient/flight/ExternalFlightService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
-public class FlightService {
+public class ExternalFlightService {
 
     @Autowired
     RestTemplate restTemplate;
