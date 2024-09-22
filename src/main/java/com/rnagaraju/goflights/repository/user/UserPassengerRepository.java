9fab34d8e4bba5722f0fe1963137ecf19bf29b5d
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2024-09-22 13:30:57 -0500

    Delete all user, airline and specific un-necessary packages and classes

--- src/main/java/com/rnagaraju/goflights/repository/user/UserPassengerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rnagaraju.goflights.repository.user;
-
-import com.rnagaraju.goflights.model.Passenger;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserPassengerRepository extends JpaRepository<Passenger, Long> {
-
-}
