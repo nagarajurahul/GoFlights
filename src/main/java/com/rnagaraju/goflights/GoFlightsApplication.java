3ba57eeb15bb1d1c7b92d3dce85411440e023899
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2025-07-19 19:19:55 -0500

    add debug to print env variables before starting application

--- src/main/java/com/rnagaraju/goflights/GoFlightsApplication.java
+++ src/main/java/com/rnagaraju/goflights/GoFlightsApplication.java
@@ -6,6 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GoFlightsApplication {
     public static void main(String[] args) {
+          // To debug and check configuration in development
+          // Never do this hasty task in production :-)
+//        System.out.println("========== EARLY ENV CHECK ==========");
+//        System.out.println("DB_URL: " + System.getenv("DB_URL"));
+//        System.out.println("DB_USER: " + System.getenv("DB_USER"));
+//        System.out.println("DB_PASSWORD: " + System.getenv("DB_PASSWORD"));
+//        System.out.println("=====================================");
+
         SpringApplication.run(GoFlightsApplication.class, args);
     }
 }
